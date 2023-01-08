4a6d3edabf328a65ba87b448ea096f88680f5cbf
Lapratomo24 <lapratomo24@yahoo.com>
2023-01-08 16:52:38 +0000

    add a state to app

--- sticky/src/App.js
+++ sticky/src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../src/components/Header";
 import NotesList from "./components/NotesList";
 
-const App = () => (
-  <div>
-    <Header />
-    <NotesList />
-  </div>
-);
+class App extends Component {
+
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "code",
+        description: "master frontend & backend languages",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sport",
+        description: "cycle and workout at the gym regularly",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "food",
+        description: "eat healthy food and hydrate well",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: "search for me"
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <NotesList />
+      </div>
+    )
+  }
+};
 
 export default App;
